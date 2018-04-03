a6bc55bd0aacf582dd401201fa624a93b50c178a
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-04-03 02:52:28 -0300

    Startando o servico de backgroundService depois do mapeamento dos endpoints

--- src/main/java/com/macuxi/camarao/MacuxiCamaraoApplication.java
+++ src/main/java/com/macuxi/camarao/MacuxiCamaraoApplication.java
@@ -1,13 +1,17 @@
 package com.macuxi.camarao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.macuxi.camarao.services.BackGroundService;
+
 @SpringBootApplication	
 public class MacuxiCamaraoApplication implements CommandLineRunner {
 	
-	
+	@Autowired
+	BackGroundService backgroundService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MacuxiCamaraoApplication.class, args);
@@ -15,10 +19,10 @@ public class MacuxiCamaraoApplication implements CommandLineRunner {
 	 
 	@Override
 	public void run (String... args)throws Exception{
-		
-		
-		
+		backgroundService.doInBackground();
 		
 	}
+	
+	
 }
 	
