f373a4b3b1bcdd7225d1e72a59dbd46d5d926894
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-06-04 17:26:25 -0300

    Permitindo acesso para achar ultima temperatura

--- src/main/java/com/macuxi/camarao/config/SecurityConfig.java
+++ src/main/java/com/macuxi/camarao/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS_GET = {		
 			
-			"/estados/**"
+			"/estados/**",
+			"/temperaturas",
 	};
 
 	private static final String[] PUBLIC_MATCHERS_POST = {
