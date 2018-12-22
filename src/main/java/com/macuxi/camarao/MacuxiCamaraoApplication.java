35707faa024f816f98174cbf21d21285280503ea
Kelvi Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-12-22 06:19:12 -0200

    Melhorando implementacao de rotina que preenche as medicoes com @EnableScheduling

--- src/main/java/com/macuxi/camarao/MacuxiCamaraoApplication.java
+++ src/main/java/com/macuxi/camarao/MacuxiCamaraoApplication.java
@@ -1,28 +1,19 @@
 package com.macuxi.camarao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.macuxi.camarao.services.BackGroundService;
 
-@SpringBootApplication	
-public class MacuxiCamaraoApplication implements CommandLineRunner {
-	
+@SpringBootApplication
+public class MacuxiCamaraoApplication {
+
 	@Autowired
 	BackGroundService backgroundService;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(MacuxiCamaraoApplication.class, args);
 	}
-	 
-	@Override
-	public void run (String... args)throws Exception{
-		backgroundService.doInBackground();
-		
-	}
-	
-	
+
 }
-	
--- src/main/java/com/macuxi/camarao/services/BackGroundService.java
+++ src/main/java/com/macuxi/camarao/services/BackGroundService.java
@@ -1,59 +1,49 @@
 package com.macuxi.camarao.services;
-import javax.swing.SwingWorker;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-    @Service
-    public class BackGroundService extends SwingWorker<Object, Object> {
-    	
-    	@Autowired
-    	private TemperaturaService temperaturaService;
-    	
-    	@Autowired
-    	private PhService phService;
-    	
-    	@Autowired
-    	SalinidadeService salinidadeService;
-    	
-    	@Autowired
-    	AmoniaTotalService amoniaTotalService;	
-    	
-    	@Autowired
-    	TransparenciaService transparenciaService;
-    	
-    	@Autowired
-    	NitritoService nitritoService;
-    	
-    	@Autowired
-    	OxigenioDissolvidoService oxigenioDissolvidoService;
-    	
-    	@Autowired
-    	NitratoService nitratoService;	
-    	
-    	
-        @Override
-		public Object doInBackground() throws Exception {
-            while (true) {
-                java.awt.EventQueue.invokeLater(new Runnable() {
-                    public void run() {
-                        temperaturaService.generateTemperatura(); 
-                        phService.generatePh();
-                        salinidadeService.generateSalinidade();
-                        amoniaTotalService.generateAmoniaTotal();
-                        transparenciaService.generateTransparencia();
-                        nitritoService.generateNitrito();
-                        oxigenioDissolvidoService.generateOxigenioDissolvido();
-                        nitratoService.generateNitrato();                        
-                    }
-                });
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+@Service
+@EnableScheduling
+public class BackGroundService {
 
-    }
+	@Autowired
+	private TemperaturaService temperaturaService;
 
+	@Autowired
+	private PhService phService;
+
+	@Autowired
+	SalinidadeService salinidadeService;
+
+	@Autowired
+	AmoniaTotalService amoniaTotalService;
+
+	@Autowired
+	TransparenciaService transparenciaService;
+
+	@Autowired
+	NitritoService nitritoService;
+
+	@Autowired
+	OxigenioDissolvidoService oxigenioDissolvidoService;
+
+	@Autowired
+	NitratoService nitratoService;
+	
+	@Scheduled(fixedDelay = 10000)
+	public void doInBackground() {
+		temperaturaService.generateTemperatura();
+		phService.generatePh();
+		salinidadeService.generateSalinidade();
+		amoniaTotalService.generateAmoniaTotal();
+		transparenciaService.generateTransparencia();
+		nitritoService.generateNitrito();
+		oxigenioDissolvidoService.generateOxigenioDissolvido();
+		nitratoService.generateNitrato();
+
+	}
+
+}
